5c258d929118bf91a57bae9052c2fc2cc06e931f
Cezary Petryka <cezaryp10@gmail.com>
2023-06-20 22:22:05 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module petryka.cezary.fallingsquaresgame {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens petryka.cezary.fallingsquaresgame to javafx.fxml;
+    exports petryka.cezary.fallingsquaresgame;
+}
---
+++ src/main/java/petryka/cezary/fallingsquaresgame/FallingSquaresGame.java
@@ -0,0 +1,17 @@
+package petryka.cezary.fallingsquaresgame;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FallingSquaresGame extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
