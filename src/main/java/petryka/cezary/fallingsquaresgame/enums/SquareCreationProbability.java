ecfdc1e1172c5e3efc95024606f1beb6856dff10
Cezary Petryka <cezaryp10@gmail.com>
2023-06-26 21:23:35 +0200

    change the value of the HIGH variable from the SquareCreationProbability enum

--- src/main/java/petryka/cezary/fallingsquaresgame/enums/SquareCreationProbability.java
+++ src/main/java/petryka/cezary/fallingsquaresgame/enums/SquareCreationProbability.java
@@ -3,7 +3,7 @@ package petryka.cezary.fallingsquaresgame.enums;
 public enum SquareCreationProbability {
     LOW(0.02),
     STANDARD(0.03),
-    HIGH(0.04);
+    HIGH(0.05);
 
     private final double probability;
 
